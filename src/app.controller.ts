3ed622678ff9c6340fd30e4c109eb845cab951b5
mavro495 <mavro@switchlabs.ai>
2025-12-05 19:22:11 +0900

    feat: init sweager

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('root')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'hello' })
+  @ApiOkResponse({
+    description: 'hello',
+    schema: {
+      example: {
+        res: 'Hello World!',
+      },
+    },
+  })
   getHello(): any {
     return this.appService.getHello();
   }
--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,10 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { HealthService } from './health.service';
 
+@ApiTags('health')
 @Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
+
   @Get()
+  @ApiOperation({ summary: 'health check' })
+  @ApiOkResponse({
+    description: 'health check',
+    schema: {
+      example: {
+        status: 'ok',
+        time: new Date().toISOString(),
+      },
+    },
+  })
   getHealth() {
     return this.healthService.getHealth();
   }
